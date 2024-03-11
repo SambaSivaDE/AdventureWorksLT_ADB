7716c7b61a4fa6dd939d205e156fa09c52550983
SambaSiva368 <SambaSiva368>
2024-03-11 15:12:55 +0000

    test_notebook
    test_notebook creations

---
+++ IncubationLabBatch01/Test_Notebook.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+display(dbutils.fs.ls("/mnt/AdventureworksLT/bronze"))
+display(dbutils.fs.ls("/mnt/AdventureworksLT/silver"))
+display(dbutils.fs.ls("/mnt/AdventureworksLT/gold"))
